6c5cede9407a556aa4b1c43e1e5d271a326e09f9
leonraz58 <leonraz58@gmail.com>
2024-03-31 20:08:07 +0300

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { Button } from '@/components/ui'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          window.location.href = 'https://google.com'
+        }}
+        variant={'primary'}
+      >
+        кнопка обычная
+      </Button>
+
+      <Button as={'a'} href={'https://google.com'} variant={'primary'}>
+        кнопка ссылка
+      </Button>
+    </div>
+  )
 }
